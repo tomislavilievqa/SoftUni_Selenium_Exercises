3de16ac1bf7dac817d5cefaa6438c2c959bf1739
Tomislav Iliev <ilievtomislav@gmail.com>
2023-06-04 09:17:14 +0300

    updating the POM tests

---
+++ DemoSeleniumWebDriver/StudentRegistryApp_Selenium_POM/Tests/AddStudentsPage.cs
@@ -0,0 +1,16 @@
+namespace StudentRegistryApp_Selenium_POM
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
